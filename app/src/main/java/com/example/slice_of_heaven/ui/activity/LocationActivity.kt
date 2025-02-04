f84aede8904e405924abce381ed940bd385f159a
aditi <adishrestha2005@gmail.com>
2025-02-04 16:56:57 +0545

    Fragmentscreated

--- app/src/main/java/com/example/slice_of_heaven/ui/activity/LocationActivity.kt
+++ app/src/main/java/com/example/slice_of_heaven/ui/activity/LocationActivity.kt
@@ -1,37 +1,17 @@
 package com.example.slice_of_heaven.ui.activity
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.slice_of_heaven.R
-import com.example.slice_of_heaven.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
-    lateinit var binding: ActivityLoginBinding
-
+class LocationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-
-        binding=ActivityLoginBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnSignUp.setOnClickListener {
-
-            val intent = Intent(
-                this@LoginActivity,
-                RegisterActivity::class.java
-            )
-            startActivity(intent)
-        }
-
-
-
-
+        setContentView(R.layout.activity_location)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
---
+++ app/src/main/java/com/example/slice_of_heaven/ui/activity/SplashscreenActivity.kt
@@ -0,0 +1,50 @@
+package com.example.slice_of_heaven.ui.activity
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.slice_of_heaven.MainActivity
+import com.example.slice_of_heaven.R
+import com.example.slice_of_heaven.databinding.ActivitySplashscreenBinding
+
+class SplashscreenActivity : AppCompatActivity() {
+
+
+        lateinit var binding: ActivitySplashscreenBinding
+        lateinit var sharedPreferences: SharedPreferences
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        binding= ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        sharedPreferences = getSharedPreferences("user", Context.MODE_PRIVATE)
+
+        val email = sharedPreferences.getString("email", "").orEmpty()
+        val password = sharedPreferences.getString("password", "").orEmpty()
+
+
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2500)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
