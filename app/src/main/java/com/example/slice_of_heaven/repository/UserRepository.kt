b558f68e56ded4bece087f72ee203253ff670905
aditi <adishrestha2005@gmail.com>
2025-02-23 14:20:36 +0545

    Userrespositoryimpl

--- app/src/main/java/com/example/slice_of_heaven/repository/UserRepository.kt
+++ app/src/main/java/com/example/slice_of_heaven/repository/UserRepository.kt
@@ -4,8 +4,9 @@ import com.example.slice_of_heaven.model.UserModel
 import com.google.firebase.auth.FirebaseUser
 
 interface UserRepository {
-    fun login(email:String,password:String,callback:(Boolean,String)->Unit)
-    fun signup(email:String,password:String,callback:(Boolean,String,String)->Unit)
+    fun login(username:String,password:String,callback:(Boolean,String)->Unit)
+    fun signup(username:String,password:String,callback:(Boolean,String,String)->Unit)
     fun addUserToDatabase(userId:String,userModel: UserModel,callback: (Boolean, String) -> Unit)
+    fun forgetPassword (username:String,callback: (Boolean, String) -> Unit)
     fun getCurretntUser(): FirebaseUser?
 }
